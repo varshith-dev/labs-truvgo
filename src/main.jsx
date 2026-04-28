457af7f4d0c048dc5a29097bd9b15117f294b2b5
Varshith-code <varshith.code@gmail.com>
2026-04-28 23:45:05 +0530

    chore: import plain CSS into main entry

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SketchProvider } from 'sketchbook-ui';
 import 'sketchbook-ui/style.css';
+import './styles.css';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
